5023a487122f8a94ae3158be0cc39c52ccda93ea
Michael Nash <michael@spatialnetworks.com>
2018-03-09 17:29:34 -0500

    first pass work in progress

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const API_KEY = process.env.API_KEY
+
+// Create a new component. This component should produce some HTML
+const App = () => {
+  return <div>Hi!</div>
+}
+console.log(API_KEY)
+console.log("Hello there!")
+// Take this components generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'))
