7433f691b6103a16466cb70eb44a80ed39665500
Michael Nash <michael@spatialnetworks.com>
2018-03-12 18:13:26 -0400

    makin' components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
 const API_KEY = 'AIzaSyCEfHxllsN9nABgjjTr6iawPQ7IJt2ljWM'
 
 // Create a new component. This component should produce some HTML
 const App = () => {
-  return <div>Hi!</div>
+  return <div>
+    <SearchBar />
+  </div>
 }
-console.log(API_KEY)
-console.log("Hello there!")
+
 // Take this components generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'))
